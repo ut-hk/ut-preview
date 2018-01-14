799752a7e6ca16c5e25c3446cd5b79187a02e499
Leo Choi <choimankin@gmail.com>
2018-01-15 00:04:42 +0800

    1. Update deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -51,7 +51,7 @@ IF NOT DEFINED KUDU_SYNC_CMD (
 IF NOT DEFINED IONIC_CMD (
   :: Install ionic
   echo Installing ionic
-  call npm install ionic -g --silent
+  call npm install ionic@latest -g --silent
   IF !ERRORLEVEL! NEQ 0 goto error
 
   :: Locally just running "kuduSync" would also work
@@ -108,9 +108,10 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 :: 2. Select node version
 call :SelectNodeVersion
 
+pushd "%DEPLOYMENT_TARGET%\temp"
+
 :: 3. Install npm packages and build
 IF EXIST "%DEPLOYMENT_TARGET%\temp\package.json" (
-  pushd "%DEPLOYMENT_TARGET%\temp"
 
   echo Installing npm packages.
   call :ExecuteCmd !NPM_CMD! install
@@ -119,13 +120,12 @@ IF EXIST "%DEPLOYMENT_TARGET%\temp\package.json" (
   call :ExecuteCmd "%IONIC_CMD%" build --prod
 
   IF !ERRORLEVEL! NEQ 0 goto error
-
-  popd
 )
 
-:: 4
-call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TARGET%\temp\www" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+:: 4. Copy
+copy www/*.* ..
 
+popd
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end

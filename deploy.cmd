a8ffcd15a8870013a5f0fb6b32fc8f918fd4c3e8
Leo Choi <choimankin@gmail.com>
2018-01-14 23:12:25 +0800

    1. Updated deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -109,18 +109,22 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 call :SelectNodeVersion
 
 :: 3. Install npm packages and build
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+IF EXIST "%DEPLOYMENT_TARGET%\temp\package.json" (
   pushd "%DEPLOYMENT_TARGET%\temp"
 
+  echo Installing npm packages.
   call :ExecuteCmd !NPM_CMD! install
-  call :ExecuteCmd !NPM_CMD! run build --prod
 
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TARGET%\temp\www" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  echo Building packages.
+  call :ExecuteCmd !NPM_CMD! run build --prod
 
   IF !ERRORLEVEL! NEQ 0 goto error
 
   popd
 )
+
+:: 4
+call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TARGET%\temp\www" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
